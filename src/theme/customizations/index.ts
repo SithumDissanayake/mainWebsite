aba11ec4902df9e978dcf2b56265f63975cc3291
Sithum Dissanayake <u7080498@anu.edu.au>
2025-02-02 15:17:03 +1100

    [NextJs-Rewrite] T3 Base

--- src/theme/customizations/index.ts
+++
@@ -1,5 +0,0 @@
-export {inputsCustomizations} from './inputs.tsx';
-export {dataDisplayCustomizations} from './dataDisplay.tsx';
-export {feedbackCustomizations} from './feedback.tsx';
-export {navigationCustomizations} from './navigation.tsx';
-export {surfacesCustomizations} from './surfaces.ts';
